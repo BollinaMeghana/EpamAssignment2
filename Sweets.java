51ace0437ed0ac3b40b37d0e550471d4f10a4551
BollinaMeghana <39005823+BollinaMeghana@users.noreply.github.com>
2020-02-06 19:16:46 +0530

    Add files via upload

---
+++ Sweets.java
@@ -0,0 +1,9 @@
+package MavenSample.EpamAssessment1;
+
+public class Sweets extends NewYearGift{
+	public Sweets(int weight , int price) {
+		w = weight ;
+		p = price ;
+		wait.add(weight) ;
+	}
+}
